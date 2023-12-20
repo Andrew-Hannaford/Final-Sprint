a4bdc4df521c6f249bf75afd75122ec1599b4608
Andrew-Hannaford <150295988+Andrew-Hannaford@users.noreply.github.com>
2023-12-20 13:36:23 -0330

    Update Goalies.js
    added a function to display into html

--- Goalies.js
+++ Goalies.js
@@ -1,6 +1,11 @@
 fetch('./GoalieStats.json')
     .then(response => response.json())
     .then(data => {
+
+        data.forEach(goalie => {
+            displayGoalieStatsInHTML(goalie);
+        });
+        
         data.forEach(goalie => {
             console.log(`
              Name: ${goalie.name}
@@ -48,4 +53,26 @@ fetch('./GoalieStats.json')
     function getWinsPerSeasonAvg(seasons, wins)  {
         winPerSeason = wins / seasons;
         return winPerSeason;
+    }
+
+    function displayGoalieStatsInHTML(goalie) {
+        const goalieStatsContainer = document.getElementById('goalieStatsContainer');
+    
+        const goalieStatsElement = document.createElement('div');
+        goalieStatsElement.innerHTML = `
+            <p>Name: ${goalie.name}</p>
+            <p>Games Played: ${goalie.gamesPlayed}</p>
+            <p>Seasons: ${goalie.seasons}</p>
+            <p>Wins per Season Average: ${getWinsPerSeasonAvg(goalie.seasons, goalie.wins).toFixed(1)}</p>
+            <p>Wins: ${goalie.wins}</p>
+            <p>Loses: ${goalie.loses}</p>
+            <p>Ties/OTL (Overtime Losses): ${getTies(goalie.wins, goalie.gamesPlayed, goalie.loses)}</p>
+            <p>Goals Against: ${goalie.goalsAgainst}</p>
+            <p>Goals Against Average: ${getGAA(goalie.goalsAgainst, goalie.timeOnIce).toFixed(2)}</p>
+            <p>Shots Against: ${goalie.shotsAgainst}</p>
+            <p>Saves: ${getSaves(goalie.goalsAgainst, goalie.shotsAgainst)}</p>
+            <p>Save Percentage: ${getSavePercentage(goalie.goalsAgainst, goalie.shotsAgainst).toFixed(3)}</p>
+            <hr>
+        `;
+        goalieStatsContainer.appendChild(goalieStatsElement)
     }
